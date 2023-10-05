8524034110f4130b1e3acc21e583a427748e9460
Paulo Adson <paulo.alves200581@gmail.com>
2023-10-04 22:47:47 -0300

    edição de cores e centralização

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Medidor de Amor Para Gatos Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Medidor de Amor Para Gatos'),
     );
   }
 }
@@ -65,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      debugPrint('debug: _counter');
     });
   }
 
@@ -106,7 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Peça para o seu gato clicar no botão, quanto mais ele clicar, maior é o Amor:',
+              style: TextStyle(color: Colors.black, fontSize: 16,), textAlign: TextAlign.center,
             ),
             Text(
               '$_counter',
@@ -118,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_box_rounded,color: Color.fromARGB(255, 255, 0, 0),),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
